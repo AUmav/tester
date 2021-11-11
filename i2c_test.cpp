6acd74acfea0641d886aaa9998d3fa0b5b3aeb28
AUmav <90242510+AUmav@users.noreply.github.com>
2021-11-11 17:12:36 +0100

    Create i2c_test.cpp

---
+++ i2c_test.cpp
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int main()
+{
+    fd = open("/dev/i2c-1", O_RDWR);
+    if (fd == -1)
+        printf("Couldn't open /i2c-1 file, error code: %d \n", errno);
+    int err = ioctl(fd, 0x0703, 0x08); // i2cdev sys call (0x0703) to set I2C addr
+    if (err != 0)
+        printf("ioctl() returns error, errorno: %d \n", errno);
+
+    while (1)
+    {
+        numRead = read(fd, &data, 2);
+        if (numRead != 2)
+            printf("Could'nt read whole buffer of data, errorcode: %d\n", errno);
+        else {
+            printf("Numbers: %d.%d\n", data[0], data[1]);
+        }
+    }
+
+
+	return 0;
+}
