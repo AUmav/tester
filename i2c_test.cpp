40d12b16c446bb71a5203f119996f8c3f69d6280
AUmav <90242510+AUmav@users.noreply.github.com>
2021-11-11 18:07:57 +0100

    Update i2c_test.cpp

--- i2c_test.cpp
+++ i2c_test.cpp
@@ -28,8 +28,8 @@ int main()
 
         sleep(1);
 
-        int numWrite = write(fd, wrData, 2);
-        if (numWrite != 2)
+        int numWrite = write(fd, wrData, 3);
+        if (numWrite != 3)
             printf("Couldn't write whole buffer of data, errorcode: %d\n", errno);
 
     }
