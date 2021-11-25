b89f18d4bd93a8694a2523dead2ec390155663e4
AUmav <90242510+AUmav@users.noreply.github.com>
2021-11-25 17:54:09 +0100

    updated i2c

--- i2c_commands.cpp
+++ i2c_commands.cpp
@@ -5,47 +5,7 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <termios.h>
-
-static struct termios old, new1;
-
-/* Initialize new terminal i/o settings */
-void initTermios(int echo)
-{
-	tcgetattr(0, &old); /* grab old terminal i/o settings */
-	new1 = old; /* make new settings same as old settings */
-	new1.c_lflag &= ~ICANON; /* disable buffered i/o */
-	new1.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-	tcsetattr(0, TCSANOW, &new1); /* use these new terminal i/o settings now */
-}
-
-/* Restore old terminal i/o settings */
-void resetTermios(void)
-{
-	tcsetattr(0, TCSANOW, &old);
-}
-
-/* Read 1 character - echo defines echo mode */
-char getch_(int echo)
-{
-	char ch;
-	initTermios(echo);
-	ch = getchar();
-	resetTermios();
-	return ch;
-}
-
-/* Read 1 character without echo */
-char getch(void)
-{
-	return getch_(0);
-}
-
-/* Read 1 character with echo */
-char getche(void)
-{
-	return getch_(1);
-}
+#include "getch.h"
 
 int main()
 {
@@ -61,8 +21,8 @@ int main()
 		printf("ioctl() returns error, errorno: %d \n", errno);
 
 
-	unsigned char rdData[rdBufSize];
-	unsigned char wrData[wrBufSize];
+	int rdData[rdBufSize];
+	int wrData[wrBufSize];
 	wrData[0] = 0; //register byte? Kan give fejl, hvis den aendres
 	while (1)
 	{
@@ -82,27 +42,10 @@ int main()
 			printf("Couldn't read whole buffer of data, errorcode: %d\n", errno);
 		}
 		else {
-			if (rdData[0] == 40) {
 				printf("Status code: %d || Data: %d\n", rdData[1], rdData[2]);
 			}
 		}*/
-		//keyhit = getch();
 	}
 
-	//printf("Exiting\n");
-
-	//wrData[1] = 16;
-	//numWrite = write(fd, wrData, wrBufSize);
-	//if (numWrite != wrBufSize)
-	//    printf("Couldn't write whole buffer (%d) of data, errorcode: %d\n", numWrite, errno);
-	//
-
-	//int numRead = read(fd, &rdData, rdBufSize);
-	//if (numRead != rdBufSize) {
-	//    printf("Couldn't read whole buffer of data, errorcode: %d\n", errno);
-	//}
-	//else {
-	//    printf("Status code: %d || Data: %d\n", rdData[1], rdData[2]);
-	//}
 	return 0;
 }
